617038da7e18ef42fd0dfa1791558cc3f05798da
hexi0721 <68277623+hexi0721@users.noreply.github.com>
2024-03-23 23:29:27 +0800

    3_23

--- Assets/Scripts/InstantiateTank.cs
+++ Assets/Scripts/InstantiateTank.cs
@@ -11,7 +11,9 @@ public class InstantiateTank : MonoBehaviour
     public Camera MapCamera; // 地圖鏡頭
     public RectTransform Map; // 地圖 canva
 
-    List<Vector3> Pos = new List<Vector3>() { new Vector3(176.63f, 40.59657f, 104.91f),  new Vector3(108f, 40.59657f, 170.5f) };
+    List<Vector3> Pos = new List<Vector3>() { new Vector3(176.63f, 40.59657f, 104.91f) , new Vector3(108f, 40.59657f, 170.5f), new Vector3(35f, 40.59657f, 142.8f) , new Vector3(47.03f , 40.59657f , 113.69f) ,
+                                              new Vector3(43.41f , 40.59657f , 80.57f) , new Vector3(43.05f , 40.59657f, 57.57f) , new Vector3(16.6f , 40.59657f , 16.6f) , new Vector3(81.06f , 40.59657f , 127.9f) , 
+                                              new Vector3(103.16f, 40.59657f , 89.44f) , new Vector3(121.5f , 40.59657f , 33.75f) };
     List<Vector3> Rot = new List<Vector3>();
 
     private void Start()
@@ -27,6 +29,7 @@ public class InstantiateTank : MonoBehaviour
         // init 待修
         for (int i = 0;i < Pos.Count;i++)
         {
+            Debug.Log(i);
             GameObject Tgo = Instantiate(EnemyTankPrefab, Pos[i], Quaternion.identity) as GameObject;
 
             GameObject EPgo = Instantiate(EnemyPoingPrefab) as GameObject;
