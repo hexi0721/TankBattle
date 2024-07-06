d4bee8ce6cfe099229d22c70bd59d876727a7d5e
hexi0721 <68277623+hexi0721@users.noreply.github.com>
2024-07-06 23:03:46 +0800

    7_6-1

--- Assets/Scripts/Typewriter.cs
+++ Assets/Scripts/Typewriter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
 
 public class Typewriter : MonoBehaviour
 {
@@ -11,16 +12,23 @@ public class Typewriter : MonoBehaviour
     public TMP_Text TextComponent;
 
     float _timer;
-    int _charIndex;
+    [SerializeField] int _charIndex;
     float _timePerChar;
-    string _currentMsg;
-    int _msgIndex;
+    [SerializeField] string _currentMsg;
+    [SerializeField] int _msgIndex;
+    bool _done;
 
     List<string> _messages;
 
     private void Awake()
     {
         _instance = this;
+
+        _messages = new List<string>();
+        _messages.Add("與友軍順利突破包圍網後，你與小隊成員在原地稍作休整，不過根據出去偵查的成員回來匯報，於東方山脈上發現有座敵軍坦克工廠。\r\n\r\n");
+        _messages.Add("刻不容緩之際，你未將這層消息上報至最高指揮部，而是兵貴神速率領小隊突襲，若成功則將為前線友軍緩解龐大壓力，即便失敗也能作為誘餌，只是你的小隊將獨自面對源源不絕的敵軍。\r\n");
+        _msgIndex = 0;
+        _currentMsg = _messages[_msgIndex];
     }
 
     private void Start()
@@ -28,31 +36,27 @@ public class Typewriter : MonoBehaviour
         _timer = 0;
         _charIndex = 0;
         _timePerChar = 0.05f;
-        
-        _messages = new List<string>();
-        _messages.Add("與友軍順利突破包圍網後，你與小隊成員在原地稍作休整，不過根據出去偵查的成員回來匯報，於東方山脈上發現有座敵軍坦克工廠。\r\n\r\n刻不容緩之際，你未將這層消息上報至最高指揮部，而是兵貴神速率領小隊突襲，若成功則將為前線友軍緩解龐大壓力，即便失敗也能作為誘餌，只是你的小隊將獨自面對源源不絕的敵軍。\r\n");
-        _msgIndex = 0;
-        _currentMsg = _messages[_msgIndex];
+        _done = false;
     }
 
     private void Update()
     {
 
-
-        if (string.IsNullOrEmpty(_currentMsg))
+        if (_msgIndex >= _messages.Count)
         {
             return;
         }
 
-        if (_charIndex >= _messages[_msgIndex].Length)
+        if (_done)
         {
-            _charIndex = 0;
-            _msgIndex += 1;
+            _done = false;
+            _msgIndex++;
             if (_msgIndex >= _messages.Count)
             {
                 return;
             }
             _currentMsg += _messages[_msgIndex];
+            
         }
 
         _timer -= Time.deltaTime;
@@ -62,18 +66,36 @@ public class Typewriter : MonoBehaviour
             _charIndex += 1;
             _timer += _timePerChar;
 
-
             string _tmpMsg = _currentMsg.Substring(0 , _charIndex);
             TextComponent.text = _tmpMsg;
         }
 
         if (_charIndex >= _currentMsg.Length)
         {
-            _currentMsg = null;
+            _done = true;
         }
         
 
     }
 
+    bool IsActive()
+    {
+        Debug.Log(_charIndex < _currentMsg.Length);
+        return _charIndex < _currentMsg.Length;
+    }
+
+    public void WriteNextMessageInQueue()
+    {
+
+        if(IsActive())
+        {
+            TextComponent.text = _currentMsg;
+            _charIndex = _currentMsg.Length;
+
+            
+            _done = true;
+        }
+
+    }
 
 }
